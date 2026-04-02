80f1937ffbdaa5928ef3fec7b3bc097f998509e5
Anduin Xue <anduin@aiursoft.com>
2026-04-02 08:07:24 +0000

    Add new SVG icons for various applications
    - Added koel.svg: Icon representing the Koel application.
    - Added memos.svg: HTML structure for the Memos application.
    - Added mth.svg: Icon for the MTH application.
    - Added ollamagateway.svg: Icon for the Olla Gateway application.
    - Added polls.svg: Icon for the Polls application.
    - Added stathub.svg: Icon for the StatHub application.
    - Added warp.svg: Icon for the Warp application.

---
+++ src/Aiursoft.CorpHome/Views/Shared/Components/SubstratumEcosystem/SubstratumEcosystem.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aiursoft.CorpHome.Views.Shared.Components.SubstratumEcosystem;
+
+public class SubstratumEcosystem : ViewComponent
+{
+    public IViewComponentResult Invoke() => View();
+}
