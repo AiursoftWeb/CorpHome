d73e0ebe132a40405b2f177720cb11e5a7dfd216
Anduin Xue <anduin@aiursoft.com>
2026-04-02 08:19:36 +0000

    feat: Add Substratum comparison component and update pricing details

---
+++ src/Aiursoft.CorpHome/Views/Shared/Components/SubstratumComparison/SubstratumComparison.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aiursoft.CorpHome.Views.Shared.Components.SubstratumComparison;
+
+public class SubstratumComparison : ViewComponent
+{
+    public IViewComponentResult Invoke() => View();
+}
